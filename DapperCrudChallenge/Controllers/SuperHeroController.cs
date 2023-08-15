edd5ee534d0bfe92452aa5fa90036b6a0768bee9
CAMUSIN <fergg8704@gmail.com>
2023-08-15 00:00:01 -0600

    Add project files.

---
+++ DapperCrudChallenge/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using DapperCrudChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCrudChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAll() 
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("select * from superheroes");
+            return Ok(heroes);
+        }
+    }
+}
